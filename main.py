1f0909c59ffc7ada2d27837243084d9f63aaed93
Justin Mao <justinmao@live.com>
2016-08-17 18:53:46 -0400

    simple rgb occurrence finder

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from PIL import Image
 
+# takes an rgb tuple (255, 255, 255)
+# returns a hex string FFFFFF
+def rgb_to_hex(rgb):
+    hex_string = ""
+    for dec in rgb:
+        hex_string += hex(dec).split("x")[1]
+    return hex_string
 
-def get_main_colors(file):
+# takes a file and finds the n top occurring RGB values specified
+# returns a list of tuples [((255, 255, 255), FFFFFF, % occurrence), ...]
+def run(file, n):
     im = Image.open(file)
-    colors = im.getcolors(im.size[0]*im.size[1])
+    colors = im.getcolors(im.size[0] * im.size[1])
     max_occ = 0
-    main_color = 0
-    secondary_color = 0
-    tertiary_color = 0
+    li = []
 
     for c in colors:
         if c[0] > max_occ:
-            tertiary_color = secondary_color
-            secondary_color = main_color
-            (max_occ, main_color) = c
-    print "Main color: " + str(main_color)
-    if secondary_color != 0:
-        print "Secondary color: " + str(secondary_color)
-        if tertiary_color != 0:
-            print "Tertiary color: " + str(tertiary_color)
+            li.insert(0, (c[1], rgb_to_hex(c[1]), round(100.0*c[0]/len(colors), 2)))
+            max_occ = c[0]
+
+    i = 0
+    while i < n:
+        print li[i]
+        i += 1
 
 image_file = raw_input("File name: ")
-get_main_colors(image_file)
+n = input("Number of top colors: ")
+run(image_file, n)
